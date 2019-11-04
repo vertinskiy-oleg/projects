9fb82f2dc7b2aef59c41c90b00e4dc96bff5d1aa
Administrator <administrator@Administrators-MacBook-Pro.local>
2019-11-04 14:46:52 +0200

    currencies.py

--- currency-tg-bot/currencies.py
+++ currency-tg-bot/currencies.py
@@ -1,5 +1,7 @@
 import requests
-import bs4
+from bs4 import BeautifulSoup
+from selenium import webdriver
+from time import sleep
 
 nbu_current_url = 'https://bank.gov.ua/NBUStatService/v1/statdirectory/exchange?json'
 privat_current_nal_url = 'https://api.privatbank.ua/p24api/pubinfo?json&exchange&coursid=5'
@@ -7,6 +9,7 @@ privat_current_beznal_url = 'https://api.privatbank.ua/p24api/pubinfo?exchange&j
 mono_current_url = 'https://api.monobank.ua/bank/currency'
 privat24_fiz_url = 'https://www.privat24.ua/'
 privat24_biz_url = 'https://otp24.privatbank.ua/'
+minfin_url = 'https://minfin.com.ua/currency/'
 
 def get_url(url):
     res = requests.get(url)
@@ -39,4 +42,14 @@ def get_mono(code='USD'):
         if d['currencyCodeA'] == iso_code[code]:
             return f"Mono Buy {code}: {round(float(d['rateBuy']), 2)}, Sale {code}: {round(float(d['rateSell']), 2)}"
 
-print(get_mono())
+def get_privat24_biz():
+
+    driver = webdriver.Chrome('./chromedriver')
+    driver.get('https://otp24.privatbank.ua/')
+    # sleep(5)
+    html = driver.page_source
+    soup = BeautifulSoup(html, 'html.parser')
+    rates = soup.select('#root > section > header > div > div > section > div:nth-child(1) > span.rate')
+    return rates[0].get_text()
+
+print(get_privat24_biz())
