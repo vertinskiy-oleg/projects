24efda5dedecd471623d0d2cd1f0ba8a430f51b0
Oleg <decruel89@gmail.com>
2019-11-25 17:01:48 +0200

    seo-cpc-tool

--- seo-cpc-tool/gsc-parser.py
+++ seo-cpc-tool/gsc-parser.py
@@ -24,21 +24,18 @@ def gsc_get_all_queries(start_date=START_DATE, end_date=END_DATE):
         'rowLimit': 10,
         'startRow': 0
     }
-    result = gsc_execute(request)
-    return result
+    return gsc_execute(request)
 
-print(gsc_get_all_queries())
+result = gsc_get_all_queries()
 
+gsheet_body = {
+        'values': [
+        ['Query', 'Clicks', 'Impressions', 'CTR', 'Avg. Position']
+        ]
+}
 
+for row in result['rows']:
+    gsheet_body['values'].append([row['keys'][0], row['clicks'], row['impressions'],
+        round(row['ctr'] * 100, 2), round(row['position'], 2)])
 
-# gsheet_body = {
-#     'values': [
-#     ['Query', 'Clicks', 'Impressions', 'CTR', 'Avg. Position']
-#     ]
-# }
-
-# for row in result['rows']:
-#     gsheet_body['values'].append([row['keys'][0], row['clicks'], row['impressions'], 
-#         round(row['ctr'] * 100, 2), round(row['position'], 2)])
-
-# gsheets_write(gsheet_body)
+gsheets_write(gsheet_body)
