baa3657a4f1e42e305403195b521f7abb74b7b7f
Administrator <administrator@Administrators-MacBook-Pro.local>
2019-12-09 16:50:44 +0200

    gsc-parser

--- seo-cpc-tool/gsc-parser.py
+++ seo-cpc-tool/gsc-parser.py
@@ -1,12 +1,13 @@
 #Сделать таблицу с Страна - URL - Запрос with Ukraine & India
 #Add Ukraine & India to Countries table
-#Optimize functions with dict.copy() and diff function parameters
+#Make code refactoring
 from googleapiclient import sample_tools
 from gsheets import gsheets_write, gsheets_clear
 
 URL = 'https://testfort.com'
 START_DATE = '2019-10-01'
 END_DATE = '2019-10-31'
+ROW_LIMIT = 3
 
 #GSC API Authorization
 def gsc_init():
@@ -21,351 +22,69 @@ def gsc_execute(body, url=URL):
     return service.searchanalytics().query(
             siteUrl=url, body=body).execute()
 
-#Getting gueries whithout Ukraine & India excluding /blog
-def gsc_get_lp_queries(start_date=START_DATE, end_date=END_DATE):
-    request = {
-        'startDate': start_date,
-        'endDate': end_date,
-        'dimensions': ['query'],
-        'rowLimit': 1000,
-        'startRow': 0,
-        "dimensionFilterGroups": [
-            {
-            "filters": [
-                {
+#General function for requesting GSC with parameters
+# Dimensions: query, page, country, device, date
+# Filters: no_ukr, no_ind, no_blog, only_blog
+def gsc_make_request(dimensions=['query'], filter=[]):
+    
+    filters = {
+        'no_ukr': {
                 "dimension": "country",
                 "operator": "notContains",
                 "expression": "ukr"
                 },
-                {
+        'no_ind': {
                 "dimension": "country",
                 "operator": "notContains",
                 "expression": "ind"
                 },
-                {
+        'no_blog': {
                 "dimension": "page",
                 "operator": "notContains",
                 "expression": "blog"
-                }
-            ]
-            }
-        ]
-    }
-
-    result = gsc_execute(request)
-    gsheet_body = {
-        'values': [
-        ['Query', 'Clicks', 'Impressions', 'CTR', 'Avg. Position']
-        ]
-    }
-
-    for row in result['rows']:
-        gsheet_body['values'].append([row['keys'][0], row['clicks'], row['impressions'],
-        round(row['ctr'] * 100, 2), round(row['position'], 2)])
-
-    gsheets_clear(range='LP Queries')
-    gsheets_write(gsheet_body, range='LP Queries')
-
-#Getting gueries whithout Ukraine & India only /blog
-def gsc_get_blog_queries(start_date=START_DATE, end_date=END_DATE):
-    request = {
-        'startDate': start_date,
-        'endDate': end_date,
-        'dimensions': ['query'],
-        'rowLimit': 1000,
-        'startRow': 0,
-        "dimensionFilterGroups": [
-            {
-            "filters": [
-                {
-                "dimension": "country",
-                "operator": "notContains",
-                "expression": "ukr"
                 },
-                {
-                "dimension": "country",
-                "operator": "notContains",
-                "expression": "ind"
-                },
-                {
+        'only_blog': {
                 "dimension": "page",
                 "operator": "contains",
                 "expression": "blog"
                 }
-            ]
-            }
-        ]
     }
-
-    result = gsc_execute(request)
-    gsheet_body = {
-        'values': [
-        ['Query', 'Clicks', 'Impressions', 'CTR', 'Avg. Position']
-        ]
-    }
-
-    for row in result['rows']:
-        gsheet_body['values'].append([row['keys'][0], row['clicks'], row['impressions'],
-        round(row['ctr'] * 100, 2), round(row['position'], 2)])
-
-    gsheets_clear(range='Blog Queries')
-    gsheets_write(gsheet_body, range='Blog Queries')
-
-#Getting urls whithout Ukraine & India excluding /blog
-def gsc_get_lp_urls(start_date=START_DATE, end_date=END_DATE):
+    
     request = {
-        'startDate': start_date,
-        'endDate': end_date,
-        'dimensions': ['page'],
-        'rowLimit': 1000,
+        'startDate': START_DATE,
+        'endDate': END_DATE,
+        'dimensions': dimensions,
+        'rowLimit': ROW_LIMIT,
         'startRow': 0,
         "dimensionFilterGroups": [
             {
-            "filters": [
-                {
-                "dimension": "country",
-                "operator": "notContains",
-                "expression": "ukr"
-                },
-                {
-                "dimension": "country",
-                "operator": "notContains",
-                "expression": "ind"
-                },
-                 {
-                "dimension": "page",
-                "operator": "notContains",
-                "expression": "blog"
-                }
-            ]
+            "filters": [filters[f] for f in filter]
             }
         ]
     }
 
     result = gsc_execute(request)
+    gsc_write_to_gsheets(dimensions, result)
+
+
+#General function for writing GSC data to GSheets
+#Sheets: LP Queries, Blog Queries, LP URL, Blog URL, LP URL-Query, 
+#Blog URL-Query, LP-Countries, Blog-Countries
+def gsc_write_to_gsheets(dimensions, result, sheet='Sheet1'):
+
     gsheet_body = {
         'values': [
-        ['URL', 'Clicks', 'Impressions', 'CTR', 'Avg. Position']
+        ['Date', *dimensions, 'Clicks', 'Impressions', 'CTR', 'Avg. Position']
         ]
     }
 
     for row in result['rows']:
-        gsheet_body['values'].append([row['keys'][0], row['clicks'], row['impressions'],
-        round(row['ctr'] * 100, 2), round(row['position'], 2)])
+        gsheet_body['values'].append([START_DATE, *row['keys'], row['clicks'], 
+        row['impressions'], round(row['ctr'] * 100, 2), round(row['position'], 2)])
 
-    gsheets_clear(range='LP URL')
-    gsheets_write(gsheet_body, range='LP URL')
+    print(gsheet_body)
+    # gsheets_clear(range=sheet)
+    # gsheets_write(gsheet_body, range=sheet)
 
-#Getting urls whithout Ukraine & India only /blog
-def gsc_get_blog_urls(start_date=START_DATE, end_date=END_DATE):
-    request = {
-        'startDate': start_date,
-        'endDate': end_date,
-        'dimensions': ['page'],
-        'rowLimit': 1000,
-        'startRow': 0,
-        "dimensionFilterGroups": [
-            {
-            "filters": [
-                {
-                "dimension": "country",
-                "operator": "notContains",
-                "expression": "ukr"
-                },
-                {
-                "dimension": "country",
-                "operator": "notContains",
-                "expression": "ind"
-                },
-                 {
-                "dimension": "page",
-                "operator": "contains",
-                "expression": "blog"
-                }
-            ]
-            }
-        ]
-    }
 
-    result = gsc_execute(request)
-    gsheet_body = {
-        'values': [
-        ['URL', 'Clicks', 'Impressions', 'CTR', 'Avg. Position']
-        ]
-    }
-
-    for row in result['rows']:
-        gsheet_body['values'].append([row['keys'][0], row['clicks'], row['impressions'],
-        round(row['ctr'] * 100, 2), round(row['position'], 2)])
-
-    gsheets_clear(range='Blog URL')
-    gsheets_write(gsheet_body, range='Blog URL')
-
-#Getting countries whithout Ukraine & India excluding /blog
-def gsc_get_lp_countries(start_date=START_DATE, end_date=END_DATE):
-    request = {
-        'startDate': start_date,
-        'endDate': end_date,
-        'dimensions': ['country'],
-        'rowLimit': 1000,
-        'startRow': 0,
-        "dimensionFilterGroups": [
-            {
-            "filters": [
-                {
-                "dimension": "country",
-                "operator": "notContains",
-                "expression": "ukr"
-                },
-                {
-                "dimension": "country",
-                "operator": "notContains",
-                "expression": "ind"
-                },
-                {
-                "dimension": "page",
-                "operator": "notContains",
-                "expression": "blog"
-                }
-            ]
-            }
-        ]
-    }
-
-    result = gsc_execute(request)
-    gsheet_body = {
-        'values': [
-        ['URL', 'Clicks', 'Impressions', 'CTR', 'Avg. Position']
-        ]
-    }
-
-    for row in result['rows']:
-        gsheet_body['values'].append([row['keys'][0], row['clicks'], row['impressions'],
-        round(row['ctr'] * 100, 2), round(row['position'], 2)])
-
-    gsheets_clear(range='LP-Countries')
-    gsheets_write(gsheet_body, range='LP-Countries')
-
-#Getting countries whithout Ukraine & India only /blog
-def gsc_get_blog_countries(start_date=START_DATE, end_date=END_DATE):
-    request = {
-        'startDate': start_date,
-        'endDate': end_date,
-        'dimensions': ['country'],
-        'rowLimit': 1000,
-        'startRow': 0,
-        "dimensionFilterGroups": [
-            {
-            "filters": [
-                {
-                "dimension": "country",
-                "operator": "notContains",
-                "expression": "ukr"
-                },
-                {
-                "dimension": "country",
-                "operator": "notContains",
-                "expression": "ind"
-                },
-                {
-                "dimension": "page",
-                "operator": "notContains",
-                "expression": "blog"
-                }
-            ]
-            }
-        ]
-    }
-
-    result = gsc_execute(request)
-    gsheet_body = {
-        'values': [
-        ['URL', 'Clicks', 'Impressions', 'CTR', 'Avg. Position']
-        ]
-    }
-
-    for row in result['rows']:
-        gsheet_body['values'].append([row['keys'][0], row['clicks'], row['impressions'],
-        round(row['ctr'] * 100, 2), round(row['position'], 2)])
-
-    gsheets_clear(range='Blog-Countries')
-    gsheets_write(gsheet_body, range='Blog-Countries')
-
-#Getting urls & queries whithout Ukraine & India excluding /blog
-def gsc_get_lp_urls_with_queries(start_date=START_DATE, end_date=END_DATE):
-    request = {
-        'startDate': start_date,
-        'endDate': end_date,
-        'dimensions': ['page', 'query'],
-        'rowLimit': 1000,
-        'startRow': 0,
-        "dimensionFilterGroups": [
-            {
-            "filters": [
-                {
-                "dimension": "page",
-                "operator": "notContains",
-                "expression": "blog"
-                }
-            ]
-            }
-        ]
-    }
-
-    result = gsc_execute(request)
-    gsheet_body = {
-        'values': [
-        ['URL', 'Query', 'Clicks', 'Impressions', 'CTR', 'Avg. Position']
-        ]
-    }
-
-    for row in result['rows']:
-        gsheet_body['values'].append([row['keys'][0], row['keys'][1], row['clicks'], row['impressions'],
-        round(row['ctr'] * 100, 2), round(row['position'], 2)])
-
-    gsheets_clear(range='LP URL-Query')
-    gsheets_write(gsheet_body, range='LP URL-Query')
-
-#Getting urls & queries whithout Ukraine & India only /blog
-def gsc_get_blog_urls_with_queries(start_date=START_DATE, end_date=END_DATE):
-    request = {
-        'startDate': start_date,
-        'endDate': end_date,
-        'dimensions': ['page', 'query'],
-        'rowLimit': 1000,
-        'startRow': 0,
-        "dimensionFilterGroups": [
-            {
-            "filters": [
-                {
-                "dimension": "page",
-                "operator": "contains",
-                "expression": "blog"
-                }
-            ]
-            }
-        ]
-    }
-
-    result = gsc_execute(request)
-    gsheet_body = {
-        'values': [
-        ['URL', 'Query', 'Clicks', 'Impressions', 'CTR', 'Avg. Position']
-        ]
-    }
-
-    for row in result['rows']:
-        gsheet_body['values'].append([row['keys'][0], row['keys'][1], row['clicks'], row['impressions'],
-        round(row['ctr'] * 100, 2), round(row['position'], 2)])
-
-    gsheets_clear(range='Blog URL-Query')
-    gsheets_write(gsheet_body, range='Blog URL-Query')
-
-gsc_get_lp_queries()
-gsc_get_blog_queries()
-gsc_get_lp_urls()
-gsc_get_blog_urls()
-gsc_get_lp_countries()
-gsc_get_blog_countries()
-gsc_get_lp_urls_with_queries()
-gsc_get_blog_urls_with_queries()
+gsc_make_request(dimensions=['query'], filter=[])
